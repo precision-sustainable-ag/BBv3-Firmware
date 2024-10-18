3d9b5683784f4be56301e2da26f9ec4d2fbc0166
djyankencsu <djyanke@ncsu.edu>
2024-10-18 10:42:39 -0400

    Preserving changes. Moving to branch for architectural work. Will resume later.

---
+++ main/toolchain.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(CMAKE_C_COMPILER "/usr/bin/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/arm-none-eabi-g++")
+set(CMAKE_ASM_COMPILER "/usr/bin/arm-none-eabi-as")
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
